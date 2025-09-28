b8299bf45077ccf326d1904b345c3070fb1e0c11
Alex Goode <alexgoodestudio@gmail.com>
2025-09-28 17:45:57 -0400

    nav banner into nav

--- src/assets/Components/Nav.jsx
+++ src/assets/Components/Nav.jsx
@@ -123,6 +123,9 @@ function Nav() {
           transition: 'all 0.5s ease-out'
         }}
       >
+                <div className="bg-slate-200 text-slate-900 text-sm text-center p-2">
+            <p className="mb-0">Dedicated to the Mental Health of Norfolk's Military Community <a href="/services" className="underline font-semibold"> Learn More</a></p>
+        </div>
         <div className="container-fluid px-4 px-lg-5">
           <div className="row align-items-center py-3">
             
